cca548bacca5827f822153dfcd07f3dc27ac6b44
Caleb Chang <changc42@gmail.com>
2020-02-14 21:25:31 -0500

    added navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Navbar from './nav'
+import Navbar from "./nav";
 
 export default class App extends Component {
   render() {
     return (
-      <div> 
-
+      <div>
+        <Navbar />
         <h1>Hello world this is the main page</h1>
       </div>
     );
