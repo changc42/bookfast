bf1f5b5999262b4dff95e9079f77062ed868b7fa
Rocky Singh <rockygsingh@icloud.com>
2020-02-14 21:22:10 -0500

    shad;fh;

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Navbar from './nav'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div> 
+
+        <h1>Hello world this is the main page</h1>
+      </div>
+    );
+  }
+}
