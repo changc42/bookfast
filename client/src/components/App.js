a8203fde1074ec5e53d2fa25098979e51f5c8d76
Caleb Chang <changc42@gmail.com>
2020-02-15 11:02:13 -0500

    created dummy bookshelf component, aded nav links, implemented react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,41 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
 import Navbar from "./nav";
-import Search from './search'
+import Search from "./search";
+import BookShelf from "./BookShelf";
 
 export default class App extends Component {
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      search: ''
-    }
+      search: ""
+    };
   }
 
   getSearchResult = result => {
-      this.setState({ search: result })
-  }
+    this.setState({ search: result });
+  };
 
-  makeAPICall = (searchTerm) => {
-
-  }
-
-
+  makeAPICall = searchTerm => {};
 
   render() {
-    console.log(this.state.search)
+    console.log(this.state.search);
     return (
-      <div>
-        <Navbar />
-        <Search getSearchResult={this.getSearchResult} />
-        <h1>Hello world this is the main page</h1>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Route exact path="/bookshelf">
+            <BookShelf />
+          </Route>
+          <Route exact path="/">
+            <div>
+              <Search getSearchResult={this.getSearchResult} />
+              <h1>Hello world this is the main page</h1>
+            </div>
+          </Route>
+        </div>
+      </BrowserRouter>
     );
   }
 }
