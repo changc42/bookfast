a9105e47ddaeec1dae5d1f9989a5d5d499009c77
Caleb Chang <changc42@gmail.com>
2020-02-14 22:22:01 -0500

    config file added

---
+++ config/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+  googleClientID:
+    "432722477779-c6oiukdfpa4urqnompk1gipdd31uoc2q.apps.googleusercontent.com",
+  googleClientSecret: "brcN83uLlt8GzkpWnWZwxqn_",
+  mongoURI:
+    "mongodb+srv://changc42:emailydev@cluster0-pqq1g.mongodb.net/test?retryWrites=true&w=majority",
+  cookieKey: "dfbvfbfdvdsvcasver",
+  stripePublishableKey: "pk_test_bjlwI3dOp4aqN8TAN9omPlji00WW7sZlO5",
+  stripeSecretKey: "sk_test_RtAwqkuZaioua0x98EqtBcl600NXz4UW7X",
+  sendGridKey:
+    "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",
+  redirectDomain: "http://localhost:3000"
+};
--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get("/", (req, res) => {
   res.send("<h1>The server is running</h1>");
 });
 
+app.get("/test", async (req, res) => {
+  let response = await axios.get(
+    "https://www.googleapis.com/books/v1/volumes?q=harry"
+  );
+  res.send("lol");
+});
+
+app.get("/sending", (req, res) => {
+  res.send("lol");
+});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
