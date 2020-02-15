067e65d23be9123f2a5be8e0696a77b0027fb1f4
Caleb Chang <changc42@gmail.com>
2020-02-15 13:03:16 -0500

    little change

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -41,7 +41,6 @@ export default class App extends Component {
                 apiCall={this.makeAPICall}
                 getSearchResult={this.getSearchResult}
               />
-              <h1>Hello world this is the main page</h1>
             </div>
           </Route>
         </div>
